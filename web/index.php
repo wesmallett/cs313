d3ed2e9378a10eb24146ab871319b60142427f10
Wes Mallett <wesmallett@Wess-MacBook-Pro.local>
2019-01-18 12:13:57 -0700

    testing something

--- web/index.php
+++ web/index.php
@@ -1,5 +1,20 @@
-<?php
+<!DOCTYPE html>
 
-  phpinfo();
-
-?>
+<html lang="en-us">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Homework</title>
+        <link rel="stylesheet" href="style.css" type="text/css">
+        <script
+        src="https://code.jquery.com/jquery-3.3.1.js"
+        integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+        crossorigin="anonymous"></script>
+        <script src="scripts.js"></script>
+    </head>
+    <body>
+        Here is the current time:
+        <?php
+            echo date("h:i:sa")
+        ?>
+    </body>
+</html>
