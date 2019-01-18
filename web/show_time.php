ec398ab6b044fcb79da62fbb8e5d6daa6aa7a6db
Wes Mallett <wesmallett@Wess-MacBook-Pro.local>
2019-01-18 14:36:57 -0700

    removed file

--- web/show_time.php
+++
@@ -1,9 +0,0 @@
- <html>
-     <body>
-
-     <?php
-        date_default_timezone_set($_GET["timezone"]);
-        echo "The time in " . $_GET["timezone"] ." is " . date("h:i:sa")
-        ?>
-    </body>
-</html>
