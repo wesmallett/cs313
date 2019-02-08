75c52b507479fbbe501ecfccefa05c13bb259302
Wes Mallett <wesmallett@Wess-MacBook-Pro.local>
2019-02-08 01:24:21 -0700

    bindparam'

--- web/data/companyData.php
+++ web/data/companyData.php
@@ -18,10 +18,18 @@ function getCompany($db,$id){
 
 function insertCompany($db,$company){
     $sql = "INSERT INTO company (companyname,streedaddress,city,state,zipcode,companywebsite,notes,pointofcontact,phonenumber,email)
-                VALUES (?,?,?,?,?,?,?,?,?,?)";
+                VALUES (:name,:address,:city,:state,:zip,:site,:notes,:poc,:phone,:email)";
     $statement = $db->prepare($sql);
-    $statement->bind_param('s,s,s,s,s,s,s,s,s,s',$company->getName(), $company->getStreetaddress(), $company->getCity(), $company->getState(), $company->getZip(),
-        $company->getWebsite(), $company->getNotes(), $company->getPointOfContact(), $company->getPhoneNumber(), $company->getEmail());
+    $statement->bindParam(':name',$company->getName());
+    $statement->bindParam(':address',$company->getStreetaddress());
+    $statement->bindParam(':city',$company->getCity());
+    $statement->bindParam(':state',$company->getState());
+    $statement->bindParam(':zip',$company->getZip());
+    $statement->bindParam(':site',$company->getWebsite());
+    $statement->bindParam(':notes',$company->getNotes());
+    $statement->bindParam(':poc',$company->getPointOfContact());
+    $statement->bindParam(':phone',$company->getPhoneNumber());
+    $statement->bindParam(':email',$company->getEmail());
     if($statement->execute()){
         error_log("SUCCESS!!!");
     }else{
