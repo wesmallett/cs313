63e2a1152a0f2ba15c4a9142ad0c62b3ab410335
Wes Mallett <wesmallett@Wess-MacBook-Pro.local>
2019-02-08 00:57:52 -0700

    changes

--- web/company/createNewCompany.php
+++ web/company/createNewCompany.php
@@ -61,6 +61,8 @@ $db = get_db();
             $company->setPhoneNumber($_POST['phont-number']);
             $company->setEmail($_POST['email']);
 
+            echo $company->getName();
+
             insertCompany($db, $company);
         }
 
