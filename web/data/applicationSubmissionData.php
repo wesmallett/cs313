fc0554a145d9a640fcd9e92f8794714b9e161821
Wes Mallett <wesmallett@Wess-MacBook-Pro.local>
2019-02-15 11:46:02 -0700

    maybe this time?

--- web/data/applicationSubmissionData.php
+++ web/data/applicationSubmissionData.php
@@ -12,9 +12,9 @@ function getSubmissionById($db,$id){
     $sql = "SELECT t1.id,t3.companyname as company, t1.jobtitle, t1.salaryrequested, t1.submissiondate, t1.resumesubmission, t1.coverlettersubmission, t1.notes, t1.linktojobposting, t2.status as applicationstatus FROM application_submissions t1 JOIN application_status t2 ON t1.applicationstatusid = t2.id JOIN company t3 ON t1.companyid = t3.id WHERE t1.id = :id";
     $statement = $db->prepare($sql);
     $statement->bindParam(':id',$id);
-    $statement->execute();
+    $rows = $statement->execute()->fetchColumn();
     $result = $statement->fetchAll(PDO::FETCH_CLASS,"ApplicationSubmission");
-    if($result->fetchColumn() > 0){
+    if($rows > 0){
         $submission = $result[0];
     }
     var_dump($submission);
