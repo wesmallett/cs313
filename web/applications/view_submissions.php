1533a6094a6222e8699932e5a09a9168f80a78b6
Wes Mallett <wesmallett@Wess-MacBook-Pro.local>
2019-02-15 00:14:55 -0700

    trying something

--- web/applications/view_submissions.php
+++ web/applications/view_submissions.php
@@ -46,7 +46,7 @@ $db = get_db();
         ?>
         <tr>
         <td><form action="submission_overview.php" method="POST">
-        <input type="hidden" id=submissionId value="<?=$submission['id']?>"/>
+        <input type="hidden" id=submissionId value="<?=$submission['submissionId']?>"/>
         <input type="submit" id="view_overview" name="insert" value="View" />
         </form></td>
         <td><?=$submission['status']?></td>
