51cc9661e1ea589365056b5bb48bff3059a1f42d
Wes Mallett <wesmallett@Wess-MacBook-Pro.local>
2019-02-15 09:22:47 -0700

    trying something

--- web/data/applicationSubmissionData.php
+++ web/data/applicationSubmissionData.php
@@ -12,8 +12,8 @@ function getSubmissionById($db,$id){
     $sql = "SELECT t1.id as submissionId,t3.companyname as company, t1.jobtitle, t1.salaryrequested, t1.submissiondate, t1.resumesubmission, t1.coverlettersubmission, t1.notes, t1.linktojobposting, t2.status as status FROM application_submissions t1 JOIN application_status t2 ON t1.applicationstatusid = t2.id JOIN company t3 ON t1.companyid = t3.id WHERE t1.id = :id";
     $statement = $db->prepare($sql);
     $statement->bind_param(':id',$id);
-    $statement->execute();
-    return $statement->fetch();
+    $statement->setFetchMode(PDO::FETCH_CLASS,"../data/applicationSubmissionData.php");
+    return  $statement->execute();
 }
 
 ?>
