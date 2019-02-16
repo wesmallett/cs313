04294697253630df8be435e0ac627caa20b742b8
Wes Mallett <wesmallett@Wess-MacBook-Pro.local>
2019-02-15 22:09:47 -0700

    new page

---
+++ web/applications/edit_submission.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Application Submissions</title>
+        <link rel="stylesheet" href="../stylings.css" type="text/css">
+    </head>
+
+    <body>
+       <?php 
+       echo $_POST['submission'];
+       ?>
+    </body>
+</html>
