5bba7a483492d3b484509f8b596ffddc83e77f56
Wes Mallett <wesmallett@Wess-MacBook-Pro.local>
2019-02-21 23:42:06 -0700

    tryagain

--- web/company/update_company.php
+++ web/company/update_company.php
@@ -15,9 +15,8 @@ $db = get_db();
 
     <body>
        <?php 
-       $submission = getCompanyById($db,intval($_POST['companyId']));
+       $company = getCompanyById($db,intval($_POST['companyId']));
        ?>
-       <label>Job Title</label><input type='text' id='job-title' value=<?=$submission->getJobtitle()?> />
-       <label>Company</label><input type='text' id='company' value=<?=$submission->getJobtitle()?> />
+       <label>Company</label><input type='text' id='company' value=<?=$company->getCompanyname()?> />
     </body>
 </html>
