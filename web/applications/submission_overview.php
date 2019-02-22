888370bcf89ad1cbf674fb99907ca16736eead20
Wes Mallett <wesmallett@Wess-MacBook-Pro.local>
2019-02-21 22:21:01 -0700

    fixing this

--- web/applications/submission_overview.php
+++ web/applications/submission_overview.php
@@ -30,7 +30,7 @@ $db = get_db();
        <?=$company->getPhonenumber()?>
        <label>Salary Requested</label><?=$submission->getSalaryrequested()?>
        <label>Notes</label><?=$submission->getNotes()?>
-
+        <h1>TEST</h1>
        <form action="edit_submission.php" method="POST">
             <!-- <input type="hidden" name=submission value=<?=$submission?> /> -->
             <input type="submit" name="edit" value='Edit Submission'/>
