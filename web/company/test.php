cd9802941b08e5481663a3fe5183057a10d8eed4
Wes Mallett <wesmallett@Wess-MacBook-Pro.local>
2019-02-22 19:40:27 -0700

    debugging

--- web/company/test.php
+++ web/company/test.php
@@ -8,9 +8,11 @@ $db = get_db();
         if(empty($_GET)){
 
         }else{
+            error_log("attempting to save");
             if($_GET['name']!=null){
+                error_log("saving");
                 $company = new Company();
-                $company->setId($_GET['id']);
+                $company->setId(intval($_GET['id']));
                 $company->setCompanyname($_GET['name']);
                 $company->setStreetaddress($_GET['street-address']);
                 $company->setCity($_GET['city']);
@@ -18,12 +20,12 @@ $db = get_db();
                 $company->setZipcode($_GET['zip']);
                 $company->setCompanywebsite($_GET['site']);
                 $company->setNotes($_GET['notes']);
-                // $company->setPointofcontact($_POST['contact']);
-                // $company->setPhonenumber($_POST['phone-number']);
-                // $company->setEmail($_POST['email']);
+                $company->setPointofcontact($_POST['poc']);
+                $company->setPhonenumber($_POST['phone']);
+                $company->setEmail($_POST['email']);
 
                 updateCompany($db, $company);
-    
+                error_log("success?");
                 header("Location: view_company.php");
                 exit();
             }else{
