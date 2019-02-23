5cb0fe107d70fcd8449f78279c0d1d203966022b
Wes Mallett <wesmallett@Wess-MacBook-Pro.local>
2019-02-22 21:26:55 -0700

    i think i got it

--- web/company/update_company.php
+++ web/company/update_company.php
@@ -33,7 +33,7 @@ $db = get_db();
     </body>
 
     <?php
-        if(isset($_POST['name'])){
+        if(!isset($_POST['name'])){
             echo "ERROR NEED NAME";
         }else{
             // $company = new Company();
