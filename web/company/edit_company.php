db6111ab401caba023557949e0796cd45646850b
Wes Mallett <wesmallett@Wess-MacBook-Pro.local>
2019-02-22 19:57:29 -0700

    i think i got it

---
+++ web/company/edit_company.php
@@ -0,0 +1,36 @@
+<?php
+require_once "../resources/dbConnect.php";
+require "../data/applicationSubmissionData.php";
+require_once "../data/companyData.php";
+require_once "../models/company.php";
+
+$db = get_db();
+        if(empty($_POST)){
+
+        }else{
+            error_log("attempting to save");
+            if($_POST['name']!=null){
+                error_log("saving");
+                $company = new Company();
+                $company->setCompanyname($_POST['name']);
+                $company->setStreetaddress($_POST['street-address']);
+                $company->setCity($_POST['city']);
+                $company->setState($_POST['state']);
+                $company->setZipcode($_POST['zip']);
+                $company->setCompanywebsite($_POST['site']);
+                $company->setNotes($_POST['notes']);
+                $company->setPointofcontact($_POST['poc']);
+                $company->setPhonenumber($_POST['phone']);
+                $company->setEmail($_POST['email']);
+                $company->setId(intval($_POST['id']));
+
+                updateCompany($db, $company);
+                error_log("success?");
+                header("Location: view_company.php");
+                exit();
+            }else{
+                echo "A Company Name is Required";
+            }
+        }        
+
+        ?>
