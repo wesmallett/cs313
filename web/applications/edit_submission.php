00d887b40e7d31ece922df6f59eaccd48a3e5f20
Wes Mallett <wesmallett@Wess-MacBook-Pro.local>
2019-02-23 00:04:18 -0700

    submission work

--- web/applications/edit_submission.php
+++ web/applications/edit_submission.php
@@ -23,13 +23,13 @@ $db = get_db();
        ?>
 
        <form action="edit_submission.php" method="POST">
-        <label>Job Title</label><input type='text' name='job-title' value=<?=$submission->getJobtitle()?> />
-        <label>Company</label><input type='text' name='company' value=<?=$submission->getJobtitle()?> />
-        <label>Salary Requested</label><input type='text' name='salary' value=<?=$submission->getSalaryrequested()?> />
-        <label>Notes</label><input type='text' name='notes' value=<?=$submission->getNotes()?> />
-        <label>Submitted Resume</label><input type='text' name='resume' value=<?=$submission->getResumesubmission()?> />
-        <label>Submitted Cover Letter</label><input type='text' name='cover-letter' value=<?=$submission->getCoverlettersubmission()?> />
-        <label>Status</label><input type='text' name='status' value=<?=$submission->getApplicationstatus()?> />
+        <label>Job Title</label><input type='text' name='job-title' value="<?=$submission->getJobtitle()?>" />
+        <label>Company</label><input type='text' name='company' value="<?=$submission->getJobtitle()?>" />
+        <label>Salary Requested</label><input type='text' name='salary' value="<?=$submission->getSalaryrequested()?>" />
+        <label>Notes</label><input type='text' name='notes' value="<?=$submission->getNotes()?>" />
+        <label>Submitted Resume</label><input type='text' name='resume' value="<?=$submission->getResumesubmission()?>" />
+        <label>Submitted Cover Letter</label><input type='text' name='cover-letter' value="<?=$submission->getCoverlettersubmission()?>" />
+        <label>Status</label><input type='text' name='status' value="<?=$submission->getApplicationstatus()?>" />
         <input type='Submit' name='save' value='Save'/>
        </form>
     </body>
